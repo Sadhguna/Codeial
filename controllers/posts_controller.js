abb90b56ca3629cc373552fdaa2c35405f768807
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-07-21 09:11:51 +0530

    created controller and router for posts

---
+++ controllers/posts_controller.js
@@ -0,0 +1,10 @@
+const { content } = require('har-validator');
+const Post = require('../models/posts');
+
+module.exports.create = function(req,res){
+    Post.create({
+        content : req.body.content,
+        user : req.user._id
+    });
+    return res.redirect('back');
+}
