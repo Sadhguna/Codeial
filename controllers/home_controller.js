c4774c3f8df7a4fad95f9a982e6dc51c56e646ad
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-07-24 22:45:17 +0530

    implemented post_controller using async-await function

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,12 @@
 const Post = require('../models/posts');
 const User = require('../models/user');
-
+// async indicates that there are asynchronous functions inside the function
 module.exports.home = async function(req,res){
     //return res.end('<h1> Express is up for Codeial!</h1>');
     //console.log(req.cookies);
     //res.cookie('user_id',50);
     try{
+        //await indicates that until the completion of post.find() it should wait and proceed to next step
         let posts = await Post.find({})
         .populate('user')
         .populate({
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,31 +2,37 @@
 const Post = require('../models/posts');
 const Comment = require('../models/comments');
 
-module.exports.create = function(req,res){
-    Post.create({
-        content : req.body.content,
-        user : req.user._id
-    });
-    return res.redirect('back');
+module.exports.create =async function(req,res){
+    try{
+        await Post.create({
+            content : req.body.content,
+            user : req.user._id
+        });
+        return res.redirect('back');
+    }
+    catch(err){
+        console.log("error",err);
+        return;
+    }
 }
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id).then((post)=>{
+module.exports.destroy = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
         // .id means converting the object id into string
         // so there is no need to write _id
         if(post.user == req.user.id){
             //let id = req.params.id;
-            Post.findByIdAndDelete(req.params.id).then().catch();
-            Comment.deleteMany({post:req.params.id}).then().catch((err)=>{
-                console.log("error in deleting post1",err);
-                return res.redirect('back');
-            });
+            await Post.findByIdAndDelete(req.params.id);
+            await Comment.deleteMany({post:req.params.id});
             return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
-    }).catch((err)=>{
-        console.log("could not able to find the post",err);
-    })
+    }
+    catch(err){
+        console.log("error",err);
+        return;
+    }
 }
