9939ff7d229211b0565a184ddf6850095f86e227
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-07-24 08:43:12 +0530

    Developed Delete and Update operations so that we can delete posts if the user is signed-in and we can delete the comments if commented user is signed-in or the user under whose post the comments are present is signed-in

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,23 @@ module.exports.create = function(req,res){
         console.log("error in adding comment to database2",err);
     });
 }
+
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id).then((comment)=>{
+        if(comment.user = req.user.id){
+            let postId = comment.post;
+            Comment.findByIdAndDelete(req.params.id).then().catch((err)=>{
+                console.log("cannot find the comment",err);
+            });
+            Post.findByIdAndUpdate(postId, {$pull : {comments : req.params.id}}).then(()=>{
+                return res.redirect('back');
+            }).catch((err)=>{
+                console.log("cannot able to find the comment in post module",err);
+            });
+            res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 //const { content } = require('har-validator');
 const Post = require('../models/posts');
+const Comment = require('../models/comments');
 
 module.exports.create = function(req,res){
     Post.create({
@@ -7,4 +8,25 @@ module.exports.create = function(req,res){
         user : req.user._id
     });
     return res.redirect('back');
+}
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id).then((post)=>{
+        // .id means converting the object id into string
+        // so there is no need to write _id
+        if(post.user == req.user.id){
+            //let id = req.params.id;
+            Post.findByIdAndDelete(req.params.id).then().catch();
+            Comment.deleteMany({post:req.params.id}).then().catch((err)=>{
+                console.log("error in deleting post1",err);
+                return res.redirect('back');
+            });
+            return res.redirect('back');
+        }
+        else{
+            return res.redirect('back');
+        }
+    }).catch((err)=>{
+        console.log("could not able to find the post",err);
+    })
 }
