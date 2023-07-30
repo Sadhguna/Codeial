2f994889e31d2d52298f42eb861fc0aeadc37a6f
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-07-30 23:37:01 +0530

    created a link in profile to upload profile photo to profile using a class called multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req,res){
     User.findById(req.params.id).then((user)=>{
@@ -34,6 +36,9 @@ module.exports.update =async function(req,res){
                 user.mail = req.body.mail;
                 //console.log(req.file);
                 if(req.file){
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname,'..',user.avatar));
+                    }
                     //this is saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename;
                 }
