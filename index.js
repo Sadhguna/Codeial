4585a3c0e4f531d068fcbb9fb6d113ec6b310c8e
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-08-11 16:52:55 +0530

    developed some css styles to the pages and created a chat box in home page

--- index.js
+++ index.js
@@ -16,18 +16,23 @@ const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const MongoStore =require('connect-mongo');
 
 const sassMiddleware = require('node-sass-middleware');
-
-const flash = require('connect-flash');
-
-const customMware = require('./config/middleware');
-
 app.use(sassMiddleware({
+    
     src : './assets/scss',
     dest : './assets/css',
     debug : true,
     outputStyle : 'extended',
     prefix : '/css'
 }));
+
+const flash = require('connect-flash');
+
+const customMware = require('./config/middleware');
+
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(charServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
 
 app.use(express.urlencoded());
 app.use(cookieParser());
