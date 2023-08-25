b5d38cac8defd8be21e3daa3a0363964b86cb0c7
pavan sadhguna attada <pavan.sadhguna@gmail.com>
2023-08-25 17:24:48 +0530

    changed ip address of chat_engine.js of assets/js folder to public ip address of our project from localhost

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -6,7 +6,7 @@ class ChatEngine{
         this.userEmail = userEmail;
         this.userName = userName;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://3.110.193.3/:5000');
 
         if (this.userEmail){
             this.connectionHandler();
